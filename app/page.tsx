3a63f4397c21b703580b58393f4a9bc86f6f4f13
Jannik Hoefener <jannik.hoefener@haw-hamburg.de>
2023-11-12 18:04:13 +0100

    api with graphql and apollo client

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,66 @@
+"use client"
 import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
+import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 
+import { gql, useQuery } from "@apollo/client";
+
+
+// http://localhost:1337/graphql
+type aboutMeResponse = {
+	aboutMe: {
+		data:{
+			attributes: aboutMeType
+		}
+	}
+}
+
+type aboutMeType = {
+	caption: string
+	aboutMe: string
+	curriculumVitae: JSON
+}
+
+const GET_ABOUTME = gql`
+query {
+	aboutMe {
+	  data {
+		attributes {
+		  caption
+		  aboutMe
+		  curriculumVitae
+		}
+	  }
+	}
+  }
+`;
+
+
 export default function Home() {
-	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-				<br />
-				<h1 className={title()}>
-					websites regardless of your design experience.
-				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
-				</h2>
-			</div>
 
-			<div className="flex gap-3">
-				<Link
-					isExternal
-					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-				>
-					Documentation
-				</Link>
-				<Link
-					isExternal
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href={siteConfig.links.github}
-				>
-					<GithubIcon size={20} />
-					GitHub
-				</Link>
-			</div>
+	// http://localhost:1337/graphql
+  const { loading, error, data } = useQuery<aboutMeResponse>(GET_ABOUTME);
+  if (loading || data === undefined) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
 
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
-			</div>
-		</section>
-	);
+  console.log(data.aboutMe.data.attributes.aboutMe)
+
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <p>
+		  {data.aboutMe.data.attributes.caption}
+	  </p>
+	  <p>
+		  {data.aboutMe.data.attributes.aboutMe}
+	  </p>
+	  <p>
+		{/* todo: how to handle the new strapi JSON text block */}
+		  {/* {data.aboutMe.data.attributes.curriculumVitae} */}
+	  </p>
+    </section>
+  );
 }
---
+++ components/linktree.tsx
@@ -0,0 +1,16 @@
+/* ToDo:
+    [] Image
+    [] Title
+    [] Tags
+    [] Description
+    [] links by .map()
+        [] IG, TT, LinkedIn, 
+        [] other web links
+*/
+import React from 'react'
+
+export default function linktree() {
+  return (
+    <div>linktree</div>
+  )
+}
