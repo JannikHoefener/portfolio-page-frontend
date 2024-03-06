a5330e05609e67fde2b719bcd395696c6f10d8fc
Jannik Hoefener <jannik.hoefener@haw-hamburg.de>
2024-03-06 12:17:29 +0100

    configure ui.aceternity

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 import {nextui} from '@nextui-org/theme'
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -12,5 +15,18 @@ module.exports = {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [nextui(),addVariablesForColors],
+}
+
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
 }
